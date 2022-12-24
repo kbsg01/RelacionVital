37094e668963b572268021c3bba293fe72d1728e
Karla Santibáñez <96139227+kbsg01@users.noreply.github.com>
2022-12-24 12:44:32 -0300

    Commit Repositories

---
+++ src/main/java/com/grupo3/proyecto/repositories/EmocionRepository.java
@@ -0,0 +1,7 @@
+package com.grupo3.proyecto.repositories;
+
+import com.grupo3.proyecto.models.Emocion;
+
+public interface EmocionRepository extends BaseRepository<Emocion>{
+    
+}
---
+++ src/main/java/com/grupo3/proyecto/repositories/UserEmocion.java
@@ -0,0 +1,8 @@
+package com.grupo3.proyecto.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserEmocion extends BaseRepository<UserEmocion>{
+    
+}
