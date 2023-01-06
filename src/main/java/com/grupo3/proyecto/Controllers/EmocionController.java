3190f53b93b590da4c2343413ad37548c7a1b1f3
Karla Santibáñez <96139227+kbsg01@users.noreply.github.com>
2023-01-06 00:33:22 -0300

    Calmate Emocion

--- src/main/java/com/grupo3/proyecto/Controllers/EmocionController.java
+++ src/main/java/com/grupo3/proyecto/Controllers/EmocionController.java
@@ -3,11 +3,17 @@ package com.grupo3.proyecto.controllers;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.grupo3.proyecto.models.Emocion;
 import com.grupo3.proyecto.models.User;
 import com.grupo3.proyecto.models.UserEmocion;
 import com.grupo3.proyecto.services.EmocionService;
@@ -27,8 +33,9 @@ public class EmocionController {
         this.uService = uService;
         this.uEService = uEService;
     }
-    
-    @GetMapping("/calendario")
+
+    // Home Emociones
+    @GetMapping("/emociones")
     public String cEmocionesHome(Model model, HttpSession session){
         Long id = (Long)session.getAttribute("userId");
         User user =  uService.findById(id);
@@ -38,5 +45,63 @@ public class EmocionController {
         return "Emociones/EmocionesHome";
     }
 
+    // Agregar Emocion
+    @GetMapping("/emocion/add")
+    public String agregarEmocion(@ModelAttribute("emocion")UserEmocion emocion, HttpSession session, Model model){
+        Long id = (Long) session.getAttribute("userId");
+        User user = uService.findById(id);
+        model.addAttribute("user", user);
+        List<Emocion> emociones = eService.findAll();
+        model.addAttribute("emociones", emociones);
+        return "Emociones/CrearEditarEmocion";
+    }
 
+    @PostMapping("/emocion/add")
+    public String agregarEmocion(@Valid @ModelAttribute("emocion")UserEmocion emocion,BindingResult result, HttpSession session, Model model){
+        Long id = (Long) session.getAttribute("userId");
+        User user = uService.findById(id);
+        model.addAttribute("user", user);
+            System.out.println(result.getAllErrors());
+        if (result.hasErrors()) {
+            List<Emocion> emociones = eService.findAll();
+            model.addAttribute("emociones", emociones);
+            return "Emociones/CrearEditarEmocion";
+        }
+        emocion.setUser(user);
+        uEService.save(emocion);
+        return "redirect:/emocion";
+    }
+
+    // Editar Emocion
+    @GetMapping("/emociones/{id}/edit")
+    public String editEmocion(@PathVariable("id")Long id, Model model, HttpSession session){
+        Long uId = (Long) session.getAttribute("userId");
+        User user = uService.findById(uId);
+        model.addAttribute("user", user);
+        UserEmocion emocion = uEService.findById(id);
+        model.addAttribute("emocion", emocion);
+        return "Emociones/CrearEditarEmocion";
+    }
+
+    @PostMapping("/emociones/{id}/edit")
+    public String editEmocion(@Valid @ModelAttribute("emocion")UserEmocion emocion, BindingResult result, Model model, HttpSession session){
+        Long uId = (Long) session.getAttribute("userId");
+        User user = uService.findById(uId);
+        model.addAttribute("user", user);
+        if (result.hasErrors()) {
+            List<Emocion> emociones = eService.findAll();
+            model.addAttribute("emociones", emociones);
+            return "Emociones/CrearEditarEmocion";
+        }
+        emocion.setUser(user);
+        uEService.save(emocion);
+        return "redirect:/emociones";
+    }
+
+    // Eliminar Emocion
+    @GetMapping("/emociones/{id}/delete")
+    public String eliminarEmocion(@PathVariable("id")Long id){
+        uEService.delete(id);
+        return "redirect:/emociones";
+    }
 }
